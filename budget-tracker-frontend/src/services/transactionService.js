ac000248dc102f97ddb5fb802379ffaa11b902fa
bilalkmalik03 <167362010+bilalkmalik03@users.noreply.github.com>
2025-04-29 16:21:23 -0700

    Fix: adding url to transactionService

--- budget-tracker-frontend/src/services/transactionService.js
+++ budget-tracker-frontend/src/services/transactionService.js
@@ -1,5 +1,4 @@
-const BASE_URL = "http://localhost:5000/api/transactions";
-
+const BASE_URL = import.meta.env.VITE_API_URL;
 export async function getTransactions(token) {
   const res = await fetch(BASE_URL, {
     headers: {
